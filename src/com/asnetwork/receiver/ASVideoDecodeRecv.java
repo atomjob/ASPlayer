276c8b882688ff8b1b991af84f9cac55391d4863
atom <atomjob@126.com>
2014-04-25 17:25:25 +0800

    delete ASPlayer when receive the stop event

--- src/com/asnetwork/receiver/ASVideoDecodeRecv.java
+++ src/com/asnetwork/receiver/ASVideoDecodeRecv.java
@@ -1,10 +1,24 @@
 package com.asnetwork.receiver;
 
+import android.util.Log;
+
+import com.asnetwork.swig.ASNativePlayer;
 import com.asnetwork.swig.ASVideoDecodeEvent;
 
 public class ASVideoDecodeRecv extends ASVideoDecodeEvent {
+	
+	public ASNativePlayer player;
+	
+	public ASVideoDecodeRecv(ASNativePlayer player){
+		this.player = player;
+	}
+	
 	public ASVideoDecodeRecv(){
 		
+	}
+	
+	public void setPlayer(ASNativePlayer player){
+		this.player = player;
 	}
 	@Override
 	public int startVideoDecoding(boolean isStart){
@@ -13,6 +27,12 @@ public class ASVideoDecodeRecv extends ASVideoDecodeEvent {
 	
 	@Override
 	public int stopVideoDecoding(boolean isStop){
+		Log.i("ASVideoDecodeRecv", "receive stopVideoDecoding event");
+		if(isStop && player!=null){
+			Log.i("ASVideoDecodeRecv", "before ASNativePlayer.releaseInstance");
+			ASNativePlayer.releaseInstance(player);
+			Log.i("ASVideoDecodeRecv", "after ASNativePlayer.releaseInstance");
+		}
 		return 0;
 	}
 }
