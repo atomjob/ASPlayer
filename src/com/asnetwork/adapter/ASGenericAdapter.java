d3053f2bfdb7134f6cfc19534c373a0025cc92b1
atom <atomjob@126.com>
2016-09-27 21:13:46 +0800

    add no sences commit

--- src/com/asnetwork/adapter/ASGenericAdapter.java
+++ src/com/asnetwork/adapter/ASGenericAdapter.java
@@ -7,7 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-
+// add no sence comments
 public abstract class ASGenericAdapter<T> extends BaseAdapter {
 	
 	public List<T> dataSource;
